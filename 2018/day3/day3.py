5c22d92750b56c561ece6dfc0049a5edca3a1462
Raj Singh <rajibsingh@gmail.com>
2018-12-09 19:07:34 -0500

    day 3 pt 1 works

--- 2018/day3/day3.py
+++ 2018/day3/day3.py
@@ -7,17 +7,30 @@ class Claim():
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
-        print("{0} {1},{2} {3},{4}".format(self.id, self.x1, self.y1, self.x2, self.y2))
+        # print("{0} {1},{2} {3},{4}".format(self.id, self.x1, self.y1, self.x2, self.y2))
 
-    def does_overlap(self, claim2):
-        if self.x2 < claim.x1:
-            return True
+    def layout(self, matrix):
+        for x in range(x1, x2):
+            for y in range(y1, y2):
+                if matrix[x][y]:
+                    matrix[x][y] = matrix[x][y]+1
+                else:
+                    matrix[x][y] = 1
 
+
+width, height = 1000, 1000;
+matrix = [[0 for x in range(width)] for y in range(height)]
 claims = []
+
+for x in range(width):
+    for y in range(height):
+        matrix[x][y] = 0
+
+
 with io.open('day3.input.txt', 'r', encoding='utf-8') as f:
     for line in f:
         tokens = line.split()
-        print(tokens)
+        # print(tokens)
         id = tokens[0][1:]
         coords = tokens[2].split(",")
         x1 = int(coords[0])
@@ -26,6 +39,16 @@ with io.open('day3.input.txt', 'r', encoding='utf-8') as f:
         x2 = x1 + int(ranges[0])
         y2 = y1 + int(ranges[1])
         claim = Claim(id=id, x1=x1, y1=y1, x2=x2, y2=y2)
+        claim.layout(matrix)
         claims.append(claim)
+
+overlap_count = 0
+for x in range(width):
+    for y in range(height):
+        if matrix[x][y]:
+           if matrix[x][y] > 1:
+               overlap_count += 1
+
+print(overlap_count)
 
 
