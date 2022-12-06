fd6ebbe6254647e358848b48c95cc5462e03572e
rajibsingh <rajibsingh@yahoo.com>
2022-12-06 17:41:13 -0500

    I get to learn about regular expressions in objectivec now

--- aoc2022/day5/day5-1.m
+++ aoc2022/day5/day5-1.m
@@ -5,41 +5,22 @@ int main() {
     NSString *fileContent = [NSString stringWithContentsOfFile:@"input.txt"
                                                       encoding:NSUTF8StringEncoding error:nil];
     NSArray *lines = [fileContent componentsSeparatedByString:(NSString *) @"\n"];
-    int overlappingRanges = 0;
-    int fl = 0; //first lower
-    int fu = 0; //first upper
-    int sl = 0; //second lower
-    int su = 0; //second upper
+
+    NSError* error = nil;
     for (NSString *line in lines) {
-        NSLog(@"** line %@", line);
-        NSMutableArray *endRanges = [[NSMutableArray alloc] init];
-        NSArray *ranges = [line componentsSeparatedByString:@","];
-        for (NSString *rangeLineStr in ranges) {
-            NSArray *rangeStrs = [rangeLineStr componentsSeparatedByString:@"-"];
-            NSNumber *lowerBound = [NSNumber numberWithInt:[(NSString *) rangeStrs[0] intValue]];
-            NSNumber *upperBound = [NSNumber numberWithInt:[(NSString *) rangeStrs[1] intValue]];
-            NSArray *range = @[lowerBound, upperBound];
-            [endRanges addObject:range];
-        }
-        NSArray *range1 = endRanges[0];
-        NSArray *range2 = endRanges[1];
-        fl = [(NSNumber *) range1[0] intValue];
-        fu = [(NSNumber *) range1[1] intValue];
-        sl = [(NSNumber *) range2[0] intValue];
-        su = [(NSNumber *) range2[1] intValue];
 
-
-        if (fu >= sl && su >= fl) {
-            overlappingRanges++;
-            NSLog(@"intersects!");
-            NSLog(@"fl: %d", fl);
-            NSLog(@"fu: %d", fu);
-            NSLog(@"sl: %d", sl);
-            NSLog(@"su: %d", su);
-        }
+        NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:@"(?:www\\.)?((?!-)[a-zA-Z0-9-]{2,63}(?<!-))\\.?((?:[a-zA-Z0-9]{2,})?(?:\\.[a-zA-Z0-9]{2,})?)" options:0 error:&error];
+        NSArray* matches = [regex matchesInString:line options:0 range:NSMakeRange(0, [line length])];
+//        for ( NSTextCheckingResult* match in matches )
+//        {
+//            NSString* matchText = [line substringWithRange:[match range]];
+//            NSLog(@"match: %@", matchText);
+//        }
+//
+//        if ([line characterAtIndex:0] == L'm') {
+//            NSLog(@"** line %@", line);
+//        }
     }
-
-    NSLog(@"%d overlapping ranges", overlappingRanges);
 
     [pool drain];
     return 0;
