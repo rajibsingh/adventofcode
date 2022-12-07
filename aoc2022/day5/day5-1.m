7e38ef17b3685aa2332fb9cb807f741ea3805993
rajibsingh <rajibsingh@yahoo.com>
2022-12-07 11:40:55 -0500

    have a working regular expression

--- aoc2022/day5/day5-1.m
+++ aoc2022/day5/day5-1.m
@@ -5,17 +5,17 @@ int main() {
     NSString *fileContent = [NSString stringWithContentsOfFile:@"input.txt"
                                                       encoding:NSUTF8StringEncoding error:nil];
     NSArray *lines = [fileContent componentsSeparatedByString:(NSString *) @"\n"];
-
     NSError* error = nil;
     for (NSString *line in lines) {
-
-        NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:@"(?:www\\.)?((?!-)[a-zA-Z0-9-]{2,63}(?<!-))\\.?((?:[a-zA-Z0-9]{2,})?(?:\\.[a-zA-Z0-9]{2,})?)" options:0 error:&error];
+        NSLog(@"line: %@", line);
+//        NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:@"(?:www\\.)?((?!-)[a-zA-Z0-9-]{2,63}(?<!-))\\.?((?:[a-zA-Z0-9]{2,})?(?:\\.[a-zA-Z0-9]{2,})?)" options:0 error:&error];
+        NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:@"\\[.\\]" options:0 error:&error];
         NSArray* matches = [regex matchesInString:line options:0 range:NSMakeRange(0, [line length])];
-//        for ( NSTextCheckingResult* match in matches )
-//        {
-//            NSString* matchText = [line substringWithRange:[match range]];
-//            NSLog(@"match: %@", matchText);
-//        }
+        for ( NSTextCheckingResult* match in matches )
+        {
+            NSString* matchText = [line substringWithRange:[match range]];
+            NSLog(@"*** match: %@", matchText);
+        }
 //
 //        if ([line characterAtIndex:0] == L'm') {
 //            NSLog(@"** line %@", line);
