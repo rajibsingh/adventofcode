1057576755017f78b0586e951d06c8334fa9f53f
rajibsingh <rajibsingh@yahoo.com>
2022-12-08 17:52:33 -0500

    run the commands, need to check if it works

--- aoc2022/day5/day5-1.m
+++ aoc2022/day5/day5-1.m
@@ -18,6 +18,7 @@ int main() {
     //parse input
     for (NSString *line in lines) {
         NSLog(@"*** line: %@", line);
+        //parsing initial setup
         NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\\[.\\]" options:0 error:&error];
         NSArray *matches = [regex matchesInString:line options:0 range:NSMakeRange(0, [line length])];
         for (NSTextCheckingResult *match in matches) {
@@ -35,13 +36,28 @@ int main() {
             [stack addObject:crateStr];
             NSLog(@"stackName: %@ holding: %d", stackName, stack.count);
         }
+
+        //handle commands
+        if ([line containsString:@"move"]) {
+            NSArray* cmdLineArray = [line componentsSeparatedByString:@" "];
+            int amount = [(NSString*)cmdLineArray[1] intValue];
+            NSString* srcStackName = (NSString*)cmdLineArray[3];
+            NSMutableArray* srcStack = stacks[srcStackName];
+            NSString* destStackName = (NSString*)cmdLineArray[5];
+            NSMutableArray* destStack = stacks[destStackName];
+            for (int i = 0; i < amount; i++) {
+                NSString* crate = (NSString*)srcStack[srcStack.count-1];
+                [srcStack removeLastObject];
+                [destStack addObject:crate];
+            }
+        }
     }
 
     // debug
     for (int i = 1; i < 10; i++) {
         NSString* stackName = [NSString stringWithFormat:@"%d",i];
         NSMutableArray* stack = stacks[stackName];
-        NSLog(@"stack %@: %d", stackName, stack.count);
+        NSLog(@"stack %@: %d: %@", stackName, stack.count, stack);
     }
 
     [pool drain];
