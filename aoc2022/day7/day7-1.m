69e07d51627e71a714a15f1e7167a6cdd1c27ac5
rajibsingh <rajibsingh@yahoo.com>
2022-12-13 16:44:34 -0500

    figuring out how to write objects in objectivec again

--- aoc2022/day7/day7-1.m
+++ aoc2022/day7/day7-1.m
@@ -1,27 +1,50 @@
 #import <Foundation/Foundation.h>
 
+@interface Tree:NSObject
+    @property (retain, nonatomic)NSString* name;
+    +(void)sampleMethod:(NSString*)greetname greeting:(NSString*)greeting;
+    -(Tree*)init:(NSString*)name;
+    -(void)pokeObject;
+@end
+
+@implementation Tree
+    +(void)sampleMethod:(NSString*)greetname greeting:(NSString*)greeting {
+       NSLog(@"%@ %@\n", greetname, greeting);
+       NSLog(@"%@n", greeting);
+    }
+
+    -(Tree*)init:(NSString* ) name {
+        if (self = [super init]) {
+            self.name = name;
+        }
+        return self;
+    }
+
+    -(void)pokeObject {
+        NSLog(@"poking object %@", self.name);
+    }
+@end
+
 int main() {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     NSString *fileContent = [NSString stringWithContentsOfFile:@"input.txt"
                                                       encoding:NSUTF8StringEncoding error:nil];
     NSArray *lines = [fileContent componentsSeparatedByString:(NSString *) @"\n"];
-
+    Tree* rootNode = [[Tree alloc] init:@"/"];
+    [Tree sampleMethod:@"raj" greeting:@"merry x-mas"];
+    [rootNode pokeObject];
+    Tree* currentLocation = nil;
     //parse input
     for (NSString *line in lines) {
         NSLog(@"\n*** line: %@", line);
-        // create data structure to hold parsed data
-        NSMutableArray *stack = [[NSMutableArray alloc] initWithCapacity:4];
-        for (int i = 0; i < line.length; i++) {
-            NSRange searchRange = NSMakeRange(i, 1);
-            NSString *retString = [line substringWithRange:searchRange];
-            [stack addObject:retString];
-            if ([stack count] > 14) {
-                [stack removeObjectAtIndex:0];
-            }
-            NSSet *set = [NSSet setWithArray:stack];
-            if ([stack count] == 14 && [set count] > 13) {
-                NSLog(@"start of packet marker found at index %d", i+1);
-                break;
+        NSArray* commandLineTokens = [line componentsSeparatedByString:@" "];
+        NSLog(@"%@", commandLineTokens);
+        NSString* command = commandLineTokens[0];
+        if ([command isEqual:@"cd"]) {
+            NSLog(@"cd command received");
+            NSString* location = commandLineTokens[1];
+            if ([location isEqual:@"/"]) {
+                currentLocation = rootNode;
             }
         }
     }
